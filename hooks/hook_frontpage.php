eedb5e8bd824f7cfc6ad79c63d26ff7ab0215e97
Ingemars Asmanis <ingemars.asmanis@notakey.com>
2019-04-05 15:58:51 +0300

    Add metadate parser XML->JSON

---
+++ hooks/hook_frontpage.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Hook to add link to the frontpage.
+ *
+ * @param array &$links  The links on the frontpage, split into sections.
+ */
+function notakey_hook_frontpage(&$links) {
+    assert('is_array($links)');
+    assert('array_key_exists("links", $links)');
+
+    $links['federation']['notakeymetacvrt'] = [
+        'href' => SimpleSAML\Module::getModuleURL('notakey/metacvrt.php'),
+        'text' => [
+            'en' => 'XML metadata to JSON converter'
+        ],
+        'shorttext' => [
+            'en' => 'Metadata converter',
+        ],
+    ];
+}
