a5923f911700d1974dca1230a2a98ca203cd323c
Ingemars Asmanis <ingemars.asmanis@notakey.com>
2019-07-01 13:51:25 +0300

    Add footer.tagline support

--- themes/ntktheme/default/includes/footer.php
+++ themes/ntktheme/default/includes/footer.php
@@ -7,6 +7,9 @@ if(!empty($this->data['htmlinject']['htmlContentPost'])) {
 ?>
 		</div>
     </div>
+    <?php if($this->configuration->hasValue('footer.tagline')){ ?>
+    <div id="footerTagline"><?php echo $this->configuration->getValue('footer.tagline'); ?> </div>
+    <?php } ?>
     <footer class="footer">
       <div class="container">
         <p><small>Copyright © <?php echo date("Y", (is_readable('/var/simplesamlphp/BUILDTS'))?@trim(file_get_contents('/var/simplesamlphp/BUILDTS')):time()); ?> Notakey Latvia SIA, v<?php echo $this->configuration->getVersion(); ?></small></p>
