bb271903d0d46c90693bbb5cb039e8243bc8a27a
Ingemars Asmanis <ingemars.asmanis@notakey.com>
2020-04-29 16:56:32 +0300

    Fix bug with profile based auth callbacks

--- www/callback.php
+++ www/callback.php
@@ -1,11 +1,11 @@
 <?php
 
 if (!isset($_REQUEST['state'])) {
-    throw new  \SimpleSAML\Error\BadRequest('Missing "state" parameter.');
+    throw new \SimpleSAML\Error\BadRequest('Missing "state" parameter.');
 }
 
 if (!isset($_REQUEST['uuid'])) {
-    throw new  \SimpleSAML\Error\BadRequest('Missing "uuid" parameter.');
+    throw new \SimpleSAML\Error\BadRequest('Missing "uuid" parameter.');
 }
 
 $auth_uuid = urldecode($_REQUEST['uuid']);
@@ -14,19 +14,19 @@ list($service_id, $auth_state, $session_id) = explode(':', urldecode($_REQUEST['
 $session = \SimpleSAML\Session::getSession($session_id);
 
 if (is_null($session)) {
-    throw new  \SimpleSAML\Error\BadRequest('Session cannot be initialized');
+    throw new \SimpleSAML\Error\BadRequest('Session cannot be initialized');
 }
 
-$state_str = $session->getData(' \SimpleSAML\Auth\State', $auth_state);
+$state_str = $session->getData('SimpleSAML_Auth_State', $auth_state);
 
 if (is_null($state_str)) {
-    throw new  \SimpleSAML\Error\BadRequest('State cannot be initialized');
+    throw new \SimpleSAML\Error\BadRequest('State cannot be initialized');
 }
 
 $state = unserialize($state_str);
 
 if ($state === false || !is_object($state['notakey:bridge'])) {
-    throw new  \SimpleSAML\Error\BadRequest('State cannot be unserialized');
+    throw new \SimpleSAML\Error\BadRequest('State cannot be unserialized');
 }
 
 $state['notakey:bridge']->setService($state, $service_id);
@@ -34,7 +34,7 @@ $state['notakey:stageOneComplete'] = true;
 $state['notakey:qruuid'] = $auth_uuid;
 
 $state_str = serialize($state);
-$session->setData(' \SimpleSAML\Auth\State', $state[\SimpleSAML\Auth\State::ID], $state_str);
+$session->setData('SimpleSAML_Auth_State', $state[\SimpleSAML\Auth\State::ID], $state_str);
 
 
 
