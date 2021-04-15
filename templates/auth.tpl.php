7c3f31db6a5e5cca534d9eff1338ab56d4c1c969
Ingemars Asmanis <ingemars.asmanis@notakey.com>
2021-04-15 13:27:25 +0300

    Fix QR related regression in form submit

--- templates/auth.tpl.php
+++ templates/auth.tpl.php
@@ -59,14 +59,10 @@ if (count($this->data['warning_messages']) > 0) { ?>
                     }
                     ?>
                     <div class="regularSubmit">
-                        <input type="button" id="regularsubmit" tabindex="4" value="<?php echo $this->t('{login:login_button}'); ?>" onClick=" this.value='<?php echo $this->t('{login:processing}'); ?>'; <?php if (!$this->data['mobile']) {
-                                                                                                                                                                                                                echo 'cancelQrQuery();';
-                                                                                                                                                                                                            } ?> this.disabled=true; this.form.submit(); return true;" />
+                        <input type="button" id="regularsubmit" tabindex="4" value="<?php echo $this->t('{login:login_button}'); ?>" onClick=" this.value='<?php echo $this->t('{login:processing}'); ?>'; this.disabled=true; this.form.submit(); return true;" />
                     </div>
                     <div class="mobileSubmit">
-                        <input type="button" tabindex="4" id="mobilesubmit" value="<?php echo $this->t('{login:login_button}'); ?>" onClick=" this.value='<?php echo $this->t('{login:processing}'); ?>'; <?php if (!$this->data['mobile']) {
-                                                                                                                                                                                                                echo 'cancelQrQuery();';
-                                                                                                                                                                                                            } ?> this.disabled=true; this.form.submit(); return true;" />
+                        <input type="button" tabindex="4" id="mobilesubmit" value="<?php echo $this->t('{login:login_button}'); ?>" onClick=" this.value='<?php echo $this->t('{login:processing}'); ?>'; this.disabled=true; this.form.submit(); return true;" />
                     </div>
                     <input type="hidden" name="service_id" value="0" />
                     <input type="hidden" name="ReturnTo" value="<?php echo htmlspecialchars($this->data['return_to']); ?>">
